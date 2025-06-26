1cc4333a003b00a3c16e7e246443ab999d0fca7f
suiyideali <suiyideali@gmail.com>
2025-06-26 13:41:10 +0800

    修改评论功能位置和IP获取位置

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: false,
-    src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: true, // Display the credit text of the banner image
@@ -44,11 +44,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    {
-      name: "GitHub",
-      url: "https://github.com/suiyideali", // Internal links should not include the base path, as it is automatically added
-      external: true, // Show an external link icon and will open in a new tab
-    },
+    // {
+    //   name: "GitHub",
+    //   url: "https://github.com/suiyideali", // Internal links should not include the base path, as it is automatically added
+    //   external: true, // Show an external link icon and will open in a new tab
+    // },
   ],
 };
 
